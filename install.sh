bea0655050320764a61ecefdbb986ca36076dc76
inotsu <126430100+GabrielLimaG3@users.noreply.github.com>
2024-07-28 19:19:53 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
+sudo git clone https://github.com/m4ll0k/SecretFinder.git
+sudo cd SecretFinder
+sudo python -m pip install -r requirements.txt
+sudo apt install figlet
+sudo mv steet-script /usr/local/bin
+sudo mv steet /usr/local/bin
 sudo go install github.com/tomnomnom/waybackurls@latest
 sudo go install github.com/lc/gau/v2/cmd/gau@latest
 sudo go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 sudo go install github.com/tomnomnom/assetfinder@latest
 sudo go install github.com/projectdiscovery/katana/cmd/katana@latest
-sudo git clone https://github.com/m4ll0k/SecretFinder.git
-sudo cd SecretFinder
-sudo python -m pip install -r requirements.txt
-cd ..
 sudo mv SecretFinder /usr/local/bin
 sudo mv /root/go/bin/assetfinder /usr/local/bin
 sudo mv /root/go/bin/gau /usr/local/bin

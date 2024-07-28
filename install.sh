2b90e125627b6632e6b3829d58264280151897e4
Gabriel <ga.lima2205@gmail.com>
2024-07-28 18:58:26 -0300

    update install

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
+sudo go install github.com/tomnomnom/waybackurls@latest
+sudo go install github.com/lc/gau/v2/cmd/gau@latest
+sudo go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+sudo go install github.com/tomnomnom/assetfinder@latest
+sudo go install github.com/projectdiscovery/katana/cmd/katana@latest
+sudo git clone https://github.com/m4ll0k/SecretFinder.git secretfinder
+sudo cd SecretFinder
+sudo python -m pip install -r requirements.txt
+cd ..
+sudo mv SecretFinder /usr/local/bin
 sudo apt install figlet
-sudo mv steet.sh /usr/local/bin
+sudo mv .steet-script /usr/local/bin
 sudo mv steet /usr/local/bin
 cd ..
 sudo rm -rf steet
+cd ..

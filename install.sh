aedb80d724816fd07b3a165b6e079a3066d9fadc
Gabriel <ga.lima2205@gmail.com>
2024-07-28 18:43:38 -0300

    add steet

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 sudo apt install figlet
-git clone https://github.com/m4ll0k/SecretFinder.git
-cd SecretFinder
-sudo python -m pip install -r requirements.txt
+sudo mv steet.sh /usr/local/bin
+sudo mv steet /usr/local/bin
 cd ..
-sudo mv SecretFinder /usr/local/bin/
+sudo rm -rf steet

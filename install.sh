de96f25f23b81ffa897ee6665521b13047254fcc
Gabriel <ga.lima2205@gmail.com>
2024-07-28 17:54:57 -0300

    first commit

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt install figlet
+git clone https://github.com/m4ll0k/SecretFinder.git
+cd SecretFinder
+python -m pip install -r requirements.txt or pip install -r requirements.txt
+cd ..
+mv SecretFinder /usr/local/bin/
---
+++ steet.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os
+import threading
+import sys
+
+os.system("figlet steet")
+print("             by inotsu\n\n")
+
+if sys.argv[1] == "-d":
+    dominio = sys.argv[2]
+    os.system(f"mkdir {dominio}")
+    threading.Thread(target=os.system(f"assetfinder {dominio} > {dominio}/assetfinder.txt"))
+    os.system(f"subfinder -silent -d {dominio} -all -o {dominio}/subfinder.txt")
+    os.system(f"sort -u {dominio}/assetfinder.txt {dominio}/subfinder.txt > {dominio}/subdominios.txt")
+elif sys.argv[1] == "-u":
+    threading.Thread(target=os.system(f"cat {sys.argv[2]} | waybackurls > wayback.txt"))
+    threading.Thread(target=os.system(f"cat {sys.argv[2]} | katana -silent -d 10 -o katana.txt"))
+    os.system(f"cat {sys.argv[2]} | gau > gau.txt")
+    os.system("sort -u gau.txt katana.txt wayback.txt > fullurls.txt")
+elif sys.argv[1] == "-js":
+    os.system(f"cat {sys.argv[2]} | while read url; do python3 /usr/local/bin/SecretFinder/SecretFinder.py -i $url -o cli; done")
+elif sys.argv[1] == "-h" or "--help":
+    print("-d         scan de subdominios")
+    print("-u         scan de urls")
+    print("-js        scan de javascript")

916bb9f9d64c2a34818cb8d5fde78c9ce0a8a9fd
Gabriel <ga.lima2205@gmail.com>
2024-08-01 11:53:40 -0300

    update install.sh

--- install.sh
+++ install.sh
@@ -9,12 +9,14 @@ sudo go install github.com/lc/gau/v2/cmd/gau@latest
 sudo go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 sudo go install github.com/tomnomnom/assetfinder@latest
 sudo go install github.com/projectdiscovery/katana/cmd/katana@latest
+sudo go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 sudo mv SecretFinder /usr/local/bin
 sudo mv /root/go/bin/assetfinder /usr/local/bin
 sudo mv /root/go/bin/gau /usr/local/bin
 sudo mv /root/go/bin/katana /usr/local/bin
 sudo mv /root/go/bin/subfinder /usr/local/bin
 sudo mv /root/go/bin/waybackurls /usr/local/bin
+sudo mv /root/go/bin/httpx /usr/local/bin
 sudo apt install figlet
 sudo mv steet-script /usr/local/bin
 sudo mv steet /usr/local/bin

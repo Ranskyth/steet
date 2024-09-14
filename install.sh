421f8c0eb49d698403dbf9cc99c228218195a953
Ranskyth <126430100+GabrielLimaG3@users.noreply.github.com>
2024-09-14 12:15:00 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,11 +8,14 @@ sudo go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 sudo go install github.com/tomnomnom/assetfinder@latest
 sudo go install github.com/projectdiscovery/katana/cmd/katana@latest
 sudo go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+sudo chmod +x hackertarget
 sudo mv SecretFinder /usr/local/bin
 sudo mv /root/go/bin/assetfinder /usr/local/bin
 sudo mv /root/go/bin/gau /usr/local/bin
 sudo mv /root/go/bin/katana /usr/local/bin
 sudo mv /root/go/bin/subfinder /usr/local/bin
 sudo mv /root/go/bin/waybackurls /usr/local/bin
+sudo mv /root/go/bin/anew /usr/local/bin
 sudo mv /root/go/bin/httpx /usr/local/bin
+sudo mv ./hackertarget /usr/local/bin
 sudo mv steet /usr/local/bin
